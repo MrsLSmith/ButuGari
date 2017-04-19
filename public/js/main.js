52bab6149b97f2cc1c0a5eef29f2a3b871cc3753
cabajian <abajianchris@gmail.com>
2017-04-19 17:32:18 -0400

    Add files via upload

--- public/js/main.js
+++ public/js/main.js
@@ -7,28 +7,24 @@ var config = {
   storageBucket: "butu-gari-1492447611966.appspot.com",
   messagingSenderId: "359268417162"
 };
-var firebase = new Firebase('https://butu-gari-1492447611966.firebaseio.com');
-
+firebase.initializeApp(config);
+var rootRef = firebase.database().ref();
 
 
 //Geolocation
 function initGeolocation()
 {
-   if( navigator.geolocation )
-   {
+   if (navigator.geolocation) {
       // Call getCurrentPosition with success and failure callbacks
       navigator.geolocation.getCurrentPosition( success, fail );
-   }
-   else
-   {
+   } else {
       alert("Sorry, your browser does not support geolocation services.");
    }
 }
-function success(position)
-{
-    document.getElementById('linkP');
-    document.getElementById('long').value = position.coords.longitude;
-    document.getElementById('lat').value = position.coords.latitude;
+function success(position) {
+    let linkP = document.getElementById('linkP');
+    let long = document.getElementById('long').value = position.coords.longitude;
+    let lat = document.getElementById('lat').value = position.coords.latitude;
 
     let link = document.createElement('a');
 	  let geoKey = "AIzaSyDXgDWfraFVxGVCyiw8TMeY_SWsS-w14tM";
@@ -37,15 +33,37 @@ function success(position)
 	  let jsonLink = "https://maps.googleapis.com/maps/api/geocode/json?latlng="+position.coords.latitude+","+position.coords.longitude+"&key="+geoKey;
 
 	  document.getElementById('map').src = mapLink;
+    document.getElementById('map').style.display = "block";
 	  link.setAttribute('href',jsonLink);
     link.innerHTML = "View location JSON";
     linkP.appendChild(link);
 }
-function fail()
-{
+function fail() {
    document.getElementById('long').value = "Cannot retrieve coordinates";
 }
 
 
-
 //Sign in
+var provider = new firebase.auth.GoogleAuthProvider();
+function googleSignin() {
+   firebase.auth()
+   .signInWithPopup(provider).then(function(result) {
+      document.getElementById('signIn').style.display = "none";
+      document.getElementById('signOut').style.display = "block";
+   }).catch(function(error) {
+      console.log(error.code)
+      console.log(error.message)
+   });
+}
+function googleSignout() {
+   firebase.auth().signOut()
+   .then(function() {
+      console.log('Signout Successfull')
+      document.getElementById('signOut').style.display = "none";
+      document.getElementById('signIn').style.display = "block";
+   }, function(error) {
+      console.log('Signout Failed')  
+   });
+}
+
+
