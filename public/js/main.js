298da490ac9868740b139353f21d08fa1909cc20
cabajian <abajianchris@gmail.com>
2017-05-02 11:59:24 -0400

    Add files via upload

--- public/js/main.js
+++ public/js/main.js
@@ -1,101 +1,172 @@
-// Initialize Firebase
+'use strict';
+
 var config = {
-  apiKey: "AIzaSyCTWzaphBeGiFTQudslGlBrQOi53Lnla20",
-  authDomain: "butu-gari-1492447611966.firebaseapp.com",
-  databaseURL: "https://butu-gari-1492447611966.firebaseio.com",
-  projectId: "butu-gari-1492447611966",
-  storageBucket: "butu-gari-1492447611966.appspot.com",
-  messagingSenderId: "359268417162"
+    apiKey: "AIzaSyCTWzaphBeGiFTQudslGlBrQOi53Lnla20",
+    authDomain: "butu-gari-1492447611966.firebaseapp.com",
+    databaseURL: "https://butu-gari-1492447611966.firebaseio.com",
+    projectId: "butu-gari-1492447611966",
+    storageBucket: "butu-gari-1492447611966.appspot.com",
+    messagingSenderId: "359268417162"
 };
 firebase.initializeApp(config);
 
+Element.prototype.remove = function() {
+    this.parentElement.removeChild(this);
+}
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+    for(var i = this.length - 1; i >= 0; i--) {
+        if(this[i] && this[i].parentElement) {
+            this[i].parentElement.removeChild(this[i]);
+        }
+    }
+}
 
-//Geolocation
-function initGeolocation()
-{
-   if (navigator.geolocation) {
-      // Call getCurrentPosition with success and failure callbacks
-      navigator.geolocation.getCurrentPosition( success, fail );
+// Initializes ButuGari.
+function ButuGari() {
+  // Shortcuts to DOM Elements.
+  this.userPic = document.getElementById('user-pic');
+  this.userName = document.getElementById('user-name');
+  this.userEmail = document.getElementById('user-email');
+  this.signInButton = document.getElementById('signInButton');
+  this.signOutButton = document.getElementById('signOutButton');
+  this.signInSnackbar = document.getElementById('must-signin-snackbar');
+  this.getGeolocationButton = document.getElementById('geolocationButton');
+  this.getConnectionsButton = document.getElementById('connectionsButton');
+  // Sign in/out handlers
+  this.signOutButton.addEventListener('click', this.signOut.bind(this));
+  this.signInButton.addEventListener('click', this.signIn.bind(this));
+  // Geolocation handler
+  this.getGeolocationButton.addEventListener('click', this.getGeolocation.bind(this));
+  // Connections handler
+  this.getConnectionsButton.addEventListener('click', this.getConnections.bind(this));
+  // Initialize firebase shortcuts & auth
+  this.initFirebase();
+}
+
+// Sets up shortcuts to Firebase features and initiate firebase auth.
+ButuGari.prototype.initFirebase = function() {
+  // Shortcuts to Firebase SDK features.
+  this.auth = firebase.auth();
+  this.database = firebase.database();
+  this.storage = firebase.storage();
+  // Initiates Firebase auth and listen to auth state changes.
+  this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
+};
+
+// Signs-in to Butu Gari.
+ButuGari.prototype.signIn = function() {
+  // Sign in Firebase using popup auth and Google as the identity provider.
+  var provider = new firebase.auth.GoogleAuthProvider();
+  this.auth.signInWithPopup(provider);
+};
+// Signs-out of Butu Gari.
+ButuGari.prototype.signOut = function() {
+  // Sign out of Firebase.
+  this.auth.signOut();
+};
+
+// Triggers when the auth state change for instance when the user signs-in or signs-out.
+ButuGari.prototype.onAuthStateChanged = function(user) {
+  if (user) { // User is signed in!
+    // Get profile pic and user's name from the Firebase user object.
+    var profilePicUrl = user.photoURL;
+    var userName = user.displayName;
+    var userEmail = user.email;
+    // Set the user's profile pic and name.
+    this.userPic.setAttribute('src', profilePicUrl || '/images/profile_placeholder.png');
+    this.userName.textContent = userName;
+    this.userName.style.display = "inline-block";
+    this.userEmail.textContent = '(' + userEmail + ')';
+    this.userEmail.style.display = "inline-block";
+    // Show user's profile and sign-out button.
+    this.userPic.style.display = "inline-block";
+    this.signOutButton.removeAttribute('hidden');
+    // Hide sign-in button.
+    this.signInButton.setAttribute('hidden', 'true');
+  } else { // User is signed out!
+    // Hide user's profile and sign-out button.
+    this.userName.style.display = "none";
+    this.userEmail.style.display = "none";
+    this.userPic.style.display = "none";
+    this.signOutButton.setAttribute('hidden', 'true');
+    // Show sign-in button.
+    this.signInButton.removeAttribute('hidden');
+    // Remove lat/long input & map iframe
+    document.getElementById('long').value = "";
+    document.getElementById('lat').value = "";
+    document.getElementById('map').style.display = "none";
+    // Remove user's lat/long
+    this.usersRef.update({
+      [this.currentUser.uid]: {
+        email: this.currentUser.email,
+        name: this.currentUser.displayName,
+        photoUrl: this.currentUser.photoURL || '/images/profile_placeholder.png'
+      }
+    });
+  } 
+};
+
+var lat, long;
+// Retrieves and sets geolocation data
+ButuGari.prototype.getGeolocation = function() {
+  if (navigator.geolocation) {
+      if (this.auth.currentUser) {
+        // Call getCurrentPosition with success and failure callbacks
+        navigator.geolocation.getCurrentPosition( success, fail );
+        this.usersRef = this.database.ref('users/');
+        this.currentUser = this.auth.currentUser;
+        // Remove json link
+        if(document.getElementById('newjsonLink')) {
+          document.getElementById('newjsonLink').remove();
+        }
+      } else {
+        alert("Please sign in before performing this action.");
+      }
    } else {
       alert("Sorry, your browser does not support geolocation services.");
    }
-}
-function success(position) {
-    let linkP = document.getElementById('linkP');
-    let long = document.getElementById('long').value = position.coords.longitude;
-    let lat = document.getElementById('lat').value = position.coords.latitude;
-    let link = document.createElement('a');
-	  let geoKey = "AIzaSyDXgDWfraFVxGVCyiw8TMeY_SWsS-w14tM";
-	  let mapKey = "AIzaSyCZgD0Sfe4nwX4ClU2nUkTBb6pgiezVyPc";
-	  let mapLink = "https://www.google.com/maps/embed/v1/place?q="+position.coords.latitude+","+position.coords.longitude+"&key="+mapKey;
-	  let jsonLink = "https://maps.googleapis.com/maps/api/geocode/json?latlng="+position.coords.latitude+","+position.coords.longitude+"&key="+geoKey;
 
-    var chrisabajianRef = firebase.database().ref("users/Chris Abajian");
-    chrisabajianRef.update({
-          long: long,
-          lat: lat
-    });
-
-    console.log("Coordinates: ");
-    console.log("Longitude: " + long);
-    console.log("Latitude: " + lat);
-	  document.getElementById('map').src = mapLink;
+   function success(position) {
+    lat = position.coords.latitude;
+    long = position.coords.longitude;
+    // Get lat/long display elements
+    document.getElementById('long').value = long;
+    document.getElementById('lat').value = lat;
+    // Get keys and create links
+    let geoKey = "AIzaSyDXgDWfraFVxGVCyiw8TMeY_SWsS-w14tM";
+    let mapKey = "AIzaSyCZgD0Sfe4nwX4ClU2nUkTBb6pgiezVyPc";
+    let mapLink = "https://www.google.com/maps/embed/v1/place?q="+lat+","+long+"&key="+mapKey;
+    let jsonLink = "https://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+long+"&key="+geoKey;
+    // Update map and json link
+    document.getElementById('map').src = mapLink;
     document.getElementById('map').style.display = "block";
-	  link.setAttribute('href',jsonLink);
-    link.innerHTML = "View location JSON";
-    linkP.appendChild(link);
-}
-function fail() {
-   document.getElementById('long').value = "Cannot retrieve coordinates";
-}
+    // Create JSON link element
+    let JSONParent = document.getElementById('JSONParent');
+    let newLink = document.createElement('a');
+    newLink.setAttribute('href',jsonLink);
+    newLink.innerHTML = "View location JSON";
+    newLink.id = "newjsonLink";
+    JSONParent.appendChild(newLink);
+    // Add a new data set entry to the Firebase Database.
+    firebase.database().ref('users/').update({
+      [firebase.auth().currentUser.uid]: {
+        lat: lat,
+        long: long,
+        email: firebase.auth().currentUser.email,
+        name: firebase.auth().currentUser.displayName,
+        photoUrl: firebase.auth().currentUser.photoURL || '/images/profile_placeholder.png'
+      }
+    });
+  }
+  function fail() {
+     document.getElementById('long').value = "Cannot retrieve coordinates";
+  }
+};
 
-
-//Sign in
-var provider = new firebase.auth.GoogleAuthProvider();
-function googleSignin() {
-   firebase.auth()
-   .signInWithPopup(provider).then(function(result) {
-      var user = result.user;
-      var name = user.displayName;
-      var email = user.email;
-
-      console.log(name + " logged in as " + email);
-
-      document.getElementById('signIn').style.display = "none";
-      document.getElementById('signOut').style.display = "inline-block";
-
-      signedIn(name, email);
-   }).catch(function(error) {
-      console.log(error.code)
-      console.log(error.message)
-   });
-}
-function googleSignout() {
-   firebase.auth().signOut()
-   .then(function() {
-      console.log('Signout Successfull')
-      document.getElementById('signOut').style.display = "none";
-      document.getElementById('signIn').style.display = "block";
-   }, function(error) {
-      console.log('Signout Failed')
-   });
-}
-function signedIn(name, email){
-  let currentUser = document.createElement('p');
-  currentUser.innerHTML = "(Signed in as " + email + ")"
-  currentUser.style.fontSize = "10px";
-  currentUser.style.display = "inline-block";
-  let container = document.getElementById('signinButtons');
-
-  container.appendChild(currentUser);
-
-  usersRef.set({
-     [name]: {
-        email: email,
-     }
-  });
+ButuGari.prototype.getConnections = function() {
+  document.write('test');
 }
 
-
-// Write to DB
-var usersRef = firebase.database().ref("users/");
+window.onload = function() {
+  window.ButuGari = new ButuGari();
+};
